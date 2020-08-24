8b4a8668993dc34075ecfd0c0b0381e675ae8d7f
Kyujin Kang <digiry@gmail.com>
2020-08-25 00:23:42 +0900

    Add main function

--- src/main.py
+++ src/main.py
@@ -33,6 +33,9 @@ class Coffee:
         self._name = menu_item.name
         self._price = menu_item.cost()
 
+    def __repr__(self) -> str:
+        return f"name:{self._name}, price: {self._price}"
+
 
 class Barista:
     def make_coffee(self, menu_item: MenuItem) -> Coffee:
@@ -44,4 +47,24 @@ class Customer:
     def order(self, menu_name: str, menu: Menu, barista: Barista):
         menu_item = menu.choose(menu_name)
         coffee = barista.make_coffee(menu_item=menu_item)
-        print(coffee)
+        print(f"Order menu name: {menu_name}")
+        print(f"Made menu - {coffee}")
+
+
+def main():
+    my_shop_menu_items = []
+    my_shop_menu_items.append(MenuItem(name="아이스아메리카노", price=3000))
+    my_shop_menu_items.append(MenuItem(name="아이스라테", price=3500))
+    my_shop_menu_items.append(MenuItem(name="아메리카노", price=2000))
+    my_shop_menu_items.append(MenuItem(name="라테", price=2500))
+
+    my_shop_menu = Menu(items=my_shop_menu_items)
+
+    customer1 = Customer()
+    barista1 = Barista()
+
+    customer1.order(menu_name="아이스아메리카노", menu=my_shop_menu, barista=barista1)
+
+
+if __name__ == "__main__":
+    main()
